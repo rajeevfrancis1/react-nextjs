5aa06281096220bf1baff84f3e15b7b8980e5d81
SPERIDIAN\rajeev.francis <rajeev.francis@speridian.com>
2025-05-03 09:19:58 -0600

    Initial setup

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,6 @@
+// app/ui/fonts.ts
+import { Lusitana, Inter, Roboto } from 'next/font/google';
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+export const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
+export const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'] });
